2bf9e3ae769fecb603efea2cc05769d944dc8904
rspears417 <racheal.spears417@gmail.com>
2018-06-12 16:34:36 -0500

    pseudo code/index linked

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+
+ 
+//Click to Start
+//Timer begins at 60 seconds and countdown
+//Player goes through all 10 questions
+//player can only guess one answer per question
+//Once completed, player submit's answers
+//HTML is updated with users score
+//Score includes: time spent, answers correct, and answers wrong */
+
